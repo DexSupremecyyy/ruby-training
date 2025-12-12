83e66bde4b87feab9bd71e92595c8e9e80db47e5
DexSupremecyyy <ahmadijaka89@gmail.com>
2025-12-12 20:23:33 +0700

    FEAT : Add validating for auth token

--- rails/latihan/my_blog/app/controllers/posts_controller.rb
+++ rails/latihan/my_blog/app/controllers/posts_controller.rb
@@ -1,28 +1,30 @@
 class PostsController < ApplicationController
+  before_action :cek_token
   before_action :set_post, only: %i[ show edit update destroy ]
+
 
   # GET /posts or /posts.json
   def index
     @posts = Post.all
   end
-
+  
   # GET /posts/1 or /posts/1.json
   def show
   end
-
+  
   # GET /posts/new
   def new
     @post = Post.new
   end
-
+  
   # GET /posts/1/edit
   def edit
   end
-
+  
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
-
+    
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: "Post was successfully created." }
@@ -33,7 +35,7 @@ class PostsController < ApplicationController
       end
     end
   end
-
+  
   # PATCH/PUT /posts/1 or /posts/1.json
   def update
     respond_to do |format|
@@ -46,25 +48,41 @@ class PostsController < ApplicationController
       end
     end
   end
-
+  
   # DELETE /posts/1 or /posts/1.json
   def destroy
     @post.destroy!
-
+    
     respond_to do |format|
       format.html { redirect_to posts_path, notice: "Post was successfully destroyed.", status: :see_other }
       format.json { head :no_content }
     end
   end
-
+  
   private
+
+    def cek_token
+      return unless request.format.json? 
+      # Return = untuk berhenti
+      # Unless = kecuali(kebalikan dari if)
+      # Keluar dari method ini kecuali kalo ada yang request ke format .json
+
+
+      if params[:token] != "Rahasia" # Validasi jika memasukan token selain "Rahasia", maka akan memunculkan Dilarang masuk dengan format .json
+
+        render json: { error: "Dilarang Masuk"}, status: :unauthorized # untuk merender error dengan kalimat "Dilarang masuk" dengan di konversikan ke format .json
+        # Untuk simbol status: :unauthorized berfungsi untuk menampilkan kode error yaitu 401 = unatuhorized
+      end
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params.expect(:id))
     end
-
+    
     # Only allow a list of trusted parameters through.
     def post_params
       params.expect(post: [ :judul, :isi ])
     end
-end
+  end
+  
