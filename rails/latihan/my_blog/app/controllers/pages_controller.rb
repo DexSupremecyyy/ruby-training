9f642cbf125fe372cdcae13cdaf19ec666da4df7
DexSupremecyyy <ahmadijaka89@gmail.com>
2025-12-13 07:54:26 +0700

    FEAT : Add button sapa

--- rails/latihan/my_blog/app/controllers/pages_controller.rb
+++ rails/latihan/my_blog/app/controllers/pages_controller.rb
@@ -2,4 +2,11 @@ class PagesController < ApplicationController
   def introduction
     
   end
+
+  def sapa_balik
+    flash[:notice] = "Yowwwww Watsappp nigg-😎 Sehat sehat ya wir😜" # Flash ini semacam penyimpanan sementara untuk notif
+    # Panggil :notice untuk memunculkan notif
+
+    redirect_to root_path # kita kasih redirect untuk memindahkan ke root
+  end
 end
